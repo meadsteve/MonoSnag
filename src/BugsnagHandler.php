8bd57c32627faa294132cf2262e9632be7a7dedb
Gallo Feliz <40487446+gallofeliz@users.noreply.github.com>
2018-10-24 14:48:16 +0200

    Added Unhandled PHP Error/exceptions

--- src/BugsnagHandler.php
+++ src/BugsnagHandler.php
@@ -27,10 +27,13 @@ class BugsnagHandler extends AbstractProcessingHandler
      */
     protected $client;
 
-    public function __construct(\Bugsnag\Client $client, $level = Logger::ERROR, $bubble = true)
+    protected $putContextInExtra;
+
+    public function __construct(\Bugsnag\Client $client, $level = Logger::ERROR, $bubble = true, $putContextInExtra = true)
     {
         parent::__construct($level, $bubble);
         $this->client = $client;
+        $this->putContextInExtra = $putContextInExtra;
     }
 
 
@@ -42,6 +45,64 @@ class BugsnagHandler extends AbstractProcessingHandler
      */
     protected function write(array $record)
     {
+        // This code is from Bugsnag\Handler
+        // We adapt calls to Bugsnag to have the same Log Behavior than Bugsnag native handler
+        $context = isset($record['context']) ? $record['context'] : array();
+
+        $isPhpError = isset($context['code']) && !empty($context['message']) && !empty($context['file']) && isset($context['line']);
+        $isUncaughtException = !empty($context['exception']) && strpos($record['message'], 'Uncaught Exception') === 0;
+
+        if ($isUncaughtException) {
+            $report = Bugsnag\Report::fromPHPThrowable(
+                $this->client->getConfig(),
+                $context['exception']
+            );
+
+            $report->setUnhandled(true);
+            $report->setSeverity('error');
+            $report->setSeverityReason([
+                'type' => 'unhandledException',
+            ]);
+
+            $this->client->notify($report);
+
+            return;
+        }
+
+        if ($isPhpError) {
+            $isFatal = strpos($record['message'], 'Fatal Error') === 0;
+
+            $report = Bugsnag\Report::fromPHPError(
+                $this->client->getConfig(),
+                $context['code'],
+                $context['message'],
+                $context['file'],
+                $context['line'],
+                $isFatal
+            );
+
+            if ($isFatal) {
+                $report->setSeverity('error');
+                $report->setSeverityReason([
+                    'type' => 'unhandledException',
+                ]);
+
+            } else {
+                $report->setSeverityReason([
+                    'type' => 'unhandledError',
+                    'attributes' => [
+                        'errorType' => Bugsnag\ErrorTypes::getName($context['code']),
+                    ],
+                ]);
+            }
+
+            $report->setUnhandled(true);
+
+            $this->client->notify($report);
+
+            return;
+        }
+
         $severity = $this->getSeverity($record['level']);
         if (isset($record['context']['exception'])) {
             $this->client->notifyException(
@@ -59,6 +120,12 @@ class BugsnagHandler extends AbstractProcessingHandler
                 (string) $record['formatted'],
                 function (\Bugsnag\Report $report) use ($record, $severity) {
                     $report->setSeverity($severity);
+                    if ($this->putContextInExtra && isset($record['context'])) {
+                        if (!isset($record['extra'])) {
+                            $record['extra'] = array();
+                        }
+                        $record['extra'] += $record['context'];
+                    }
                     if (isset($record['extra'])) {
                         $report->setMetaData($record['extra']);
                     }
